844f05614fee38f57477a01bf4dabf7f61eb2e74
ankurrai <ankur.rai@alefeducation.com>
2024-01-28 02:40:40 +0400

    Problem - 2 | LeetCode | Greatest Common Divisor of Strings | updated fast solution

--- 75-starter/GcdOfStrings.java
+++ 75-starter/GcdOfStrings.java
@@ -16,3 +16,22 @@ public class GcdOfStrings {
         return "";       
     }
 }
+
+
+// Fast solution 
+
+// if merging forward and backward are same then
+// take hcf of size of both and take substring of any is your result
+
+class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+        if(!(str1+str2).equals(str2+str1)) return "";
+        int gcdVal = gcd(str1.length(),str2.length());
+        return str2.substring(0,gcdVal);
+    }
+
+    int gcd(int x,int y){
+      if(y==0) return x;
+      else return gcd(y,x%y);
+    }
+}
